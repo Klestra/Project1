d50a0eb0bd690de92dc690d46d86227ebf653994
admin <admin@iMac3.local>
2018-04-05 16:45:37 +0200

    Added somes functionality

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
+//@TODO function gameOver not finish
+
 // Select the div via it's id and store the selected element
 // inside gameContainer var.
 let gameContainer = document.querySelector('#game-container');
+let buttonReset = document.querySelector('#button-reset');
+let childDivs = document.querySelectorAll('#game-container>div');
+//Initialisez the score at 0
+let score = 0;
 
+document.getElementById('score').innerHTML = score;
+
+
+function gameOver(div) {
+  if (score > 5) {
+    alert('Vous avez perdu');
+  } else {
+    for(let i = 0; divs.length; i++) {
+      if(divs[i].classList.contains('green-color')) {
+        victoryStatus = false;
+      }
+    }
+  }
+}
+
+//Function for restart the game
+function buttonRestart(div) {
+  let green = "green-color";
+  let red = "red-color";
+  if(div.classList.contains(red)){
+    div.classList.remove(red);
+    div.classList.add(green);
+  }
+};
+
+//Function for invert the color
 function invertColor(div) {
   let green = "green-color";
   let red = "red-color";
@@ -28,12 +60,17 @@ function checkVictory(divs) {
 };
 
 
-
+//When someone clicks on the button, the game and the score restart
+buttonReset.addEventListener('click', function() {
+  for(let i =0; i < childDivs.length; i++) {
+    buttonRestart(childDivs[i]);
+    score = 0;
+    document.getElementById('score').innerHTML = score;
+  }
+});
 // When someone clicks on anything inside the game container, it triggers
 // the anonymous function
 gameContainer.addEventListener('click', function(el) {
-  let childDivs = document.querySelectorAll('#game-container>div');
-
   // We get the target of the click event, which is the specific div and not
   // the container div
   let clickedElement = el.target;
@@ -47,14 +84,18 @@ gameContainer.addEventListener('click', function(el) {
       if(i > 0 && i < (childDivs.length -1)) {
         invertColor(childDivs[i + 1]);
         invertColor(childDivs[i - 1]);
-
+        invertColor(childDivs[i]);
       } else if (i == 0) {
         invertColor(childDivs[i + 1]);
-
+        invertColor(childDivs[i]);
       } else if (i == (childDivs.length - 1)) {
         invertColor(childDivs[i - 1]);
+        invertColor(childDivs[i]);
       }
     }
   }
+  score++;
+  document.getElementById('score').innerHTML = score;
   checkVictory(childDivs);
+  gameOver();
 });
